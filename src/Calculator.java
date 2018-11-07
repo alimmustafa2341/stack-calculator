7a8aa34f9593eb1a6492a1ad9c392fea9472bd70
Soyed Mustafa <Soyed.Mustafa@mail.bcu.ac.uk>
2018-11-07 10:34:06 +0000

    lab task

---
+++ src/Calculator.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Calculator {
+	List<Integer> calc = new ArrayList<Integer>();
+	
+
+	public void loadConstant(int i) {
+		calc.add(3);
+		
+	}
+	public string totring() {
+		
+	}
+
+
+	public void add() {
+		
+		
+	}
+
+	public void subtract() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void multiply() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void divide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void total() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
